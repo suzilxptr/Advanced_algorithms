9eaeb522536a2476e78fae4d5384bba60c9f1ff0
sushil.bastola <suzilxptr@gmail.com>
2016-12-07 19:26:57 +0200

    brute force search lab6

---
+++ algorithm_lab6/src/Item.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author The BigBang
+ */
+public class Item {
+    
+ private int value;
+ private int weight;
+ private int index;
+ 
+ 
+ public Item(int value, int weight,int index){
+     this.value=value;
+     this.weight=weight;
+     this.index=index;
+     
+ }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the weight
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return the index
+     */
+    public int getIndex() {
+        return index;
+    }
+
+    /**
+     * @param index the index to set
+     */
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    
+}
